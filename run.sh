49fb743b796b8402738605438fc7e84f8cbe6119
Vladislav <vladislavsultan846@gmail.com>
2026-05-13 09:09:52 +0300

    fully fix

--- run.sh
+++ run.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-mkdir -p /home/$USER/.config/ ;
-mv GOTOCONFIG/* /home/$USER/.config/ ;
-rmdir GOTOCONFIG/;
-rm -rf .git;
-rm .gitignore;
-mv .* /home/$USER/;
-echo 'all config files successfully added ';
-echo 'but config for web you must configure manually';
+mkdir -p /home/$USER/.config/ 
+mkdir -p /home/$USER/.local/bin 
+mv GOTOCONFIG/* /home/$USER/.config/ 
+rm -rf .git 
+rm -rf GOTOCONFIG 
+read -n 1 -p "type 'y' if you have a notebook, or 'n' if this wrong" answer
+echo ''
+if [[ "${answer,,}" == "y" ]] ; then
+    mv notebook/.[!.]* ./
+fi
+rm -rf notebook/
+mv .[!.]* /home/$USER/
+echo 'all config files successfully added '
+echo ''
+read -n 1 -p "type 'y' if you like install suckless" answer
+if [[ "${answer,,}" == "y" ]] ; then
+    ./get_dwm.sh
+fi
+read -n 1 -p "type 'y' to install Telegram" answer
+echo ''
+if [[ "${answer,,}" == "y" ]] ; then
+    wget 'https://telegram.org/dl/desktop/linux' -O telega.tar.gz
+    tar -xavf telega.tar.gz
+fi    
+echo 'but config for web you must configure manually'
