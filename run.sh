b08d9cbb69bac3fbbf7a04670a29d6a88e7126a0
Vladislav <vladislavsultan846@gmail.com>
2026-05-18 23:41:37 +0300

    fix bugs

--- run.sh
+++ run.sh
@@ -2,17 +2,20 @@
 mkdir -p /home/$USER/.config/ 
 mkdir -p /home/$USER/.local/bin 
 mv GOTOCONFIG/* /home/$USER/.config/ 
-read -n 1 -p "type 'y' if you have a notebook, or 'n' if this wrong\n" answer
+read -n 1 -p "type 'y' if you have a notebook, or 'n' if this wrong" answer
+echo ''
 if [[ "${answer,,}" == "y" ]] ; then
-    mv notebook/.[!.]* dotfiles
+    mv notebook/.* dotfiles
 fi
-mv dotfiles/* /home/$USER/
+mv dotfiles/.* /home/$USER/
 echo "all config files successfully added"
-read -n 1 -p "type 'y' if you like install suckless\n" answer
+read -n 1 -p "type 'y' if you like install suckless" answer
+echo ''
 if [[ "${answer,,}" == "y" ]] ; then
     ./get_dwm.sh
 fi
-read -n 1 -p "type 'y' to install Telegram\n" answer
+read -n 1 -p "type 'y' to install Telegram" answer
+echo ''
 if [[ "${answer,,}" == "y" ]] ; then
     wget 'https://telegram.org/dl/desktop/linux' -O telega.tar.gz
     tar -xavf telega.tar.gz
